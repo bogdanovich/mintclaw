577a3ecf66bd6f1d8d39c1a9dbcb0a39721ab963
Anton Bogdanovich <27antonb@gmail.com>
2026-08-23 23:18:42 -0700

    fix(nodes): validate companion browser runtime

--- pkg/nodes/companion/browser_directory_identity_other.go
+++ pkg/nodes/companion/browser_directory_identity_other.go
@@ -14,3 +14,7 @@ func validateBrowserProfileDirectory(os.FileInfo) error {
 func validateBrowserDriverDirectory(os.FileInfo) error {
 	return errors.New("driver directory ownership validation is unsupported on this platform")
 }
+
+func validateBrowserExecutableFile(os.FileInfo) error {
+	return errors.New("browser executable identity validation is unsupported on this platform")
+}
