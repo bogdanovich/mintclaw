a3d737e1c988833171a81196ac8dbed805dc1163
Anton Bogdanovich <27antonb@gmail.com>
2026-09-09 02:59:46 -0700

    test(coding): prove native worker recovery

--- scripts/run-go-integration-tests.sh
+++ scripts/run-go-integration-tests.sh
@@ -7,6 +7,7 @@ test_root="$(mktemp -d)"
 trap 'rm -rf "$test_root"' EXIT
 
 companion_binary="$test_root/mintclaw-node"
+coding_binary="$test_root/mintclaw"
 integration_tests='^('\
 'TestCompanionProcessAuthenticatesAndInvokesOverWSS|'\
 'TestCompanionProcessTransfersFilesOverAuthenticatedWSS|'\
@@ -15,17 +16,24 @@ integration_tests='^('\
 'TestNodeFileTransferVerticalSliceWithApprovalAndDelivery|'\
 'TestNodeJobVerticalSliceWithRestartArtifactAndCancellation|'\
 'TestNodeServiceStatusModelToSystemdRealProcessVerticalSlice|'\
-'TestDocumentPDFTelegramVerticalSlice'\
+'TestDocumentPDFTelegramVerticalSlice|'\
+'TestNativeMintClawWorkerStartsSteersResumesAndShutsDown|'\
+'TestNativeMintClawWorkerCrashReleasesLeaseWithoutBlindReplay|'\
+'TestNativeMintClawWorkerDisconnectAndHardCancelAreExplicit'\
 ')$'
 
 cd "$repository_root"
 go build -o "$companion_binary" ./cmd/mintclaw-node
+go build -tags goolm,stdjson -o "$coding_binary" ./cmd/mintclaw
 
 MINTCLAW_NODE_TEST_BINARY="$companion_binary" \
+MINTCLAW_CODING_WORKER_TEST_BINARY="$coding_binary" \
+MINTCLAW_REQUIRE_CODING_WORKER_E2E=1 \
 MINTCLAW_REQUIRE_DOCUMENT_AGENT_E2E=1 go test \
   -count=1 \
   -tags goolm,stdjson,integration \
   -run "$integration_tests" \
   ./cmd/mintclaw-node \
   ./pkg/agent \
+  ./pkg/coding/workerprocess \
   ./pkg/gateway
